996f507ecaf771ed2dada2e04af6b940b2118724
12PAIN <hexagon643@gmail.com>
2019-05-23 21:40:52 +0700

    Resolution of task

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "90-triangle:\n";
+    cout << "+\n" ;
+    cout << "++\n" ;
+    cout << "+++\n" ;
+    cout << "++++\n" ;
+    cout << "+++++\n" ;
+    cout << "++++++\n" ;
+    cout << "+++++++\n" ;
+
+
+    return 0;
+}
